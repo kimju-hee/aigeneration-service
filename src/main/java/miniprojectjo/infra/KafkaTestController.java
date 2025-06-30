e9e3a828ce1017c3ffa1c8e057128da65afb798c
JinHo Choi <choijinho321@gmail.com>
2025-06-30 05:49:01 +0000

    import fix : JINO

--- src/main/java/miniprojectjo/infra/KafkaTestController.java
+++ src/main/java/miniprojectjo/infra/KafkaTestController.java
@@ -1,11 +1,21 @@
+package miniprojectjo.infra;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import miniprojectjo.domain.PublishingRequested;
 
 @RestController
 @RequestMapping("/kafka-test")
 public class KafkaTestController {
-
+    
     @Autowired
     private KafkaTemplate<String, Object> kafkaTemplate;
-
+    
     @PostMapping("/publish")
     public String publishTestEvent(@RequestBody PublishingRequested event) {
         event.setEventType("PublishingRequested"); // 중요: 헤더 대신 객체 속성으로 처리
