4340f94324f6bc8ed210a4e19145ecb0ea378c25
JinHo Choi <choijinho321@gmail.com>
2025-06-30 07:04:28 +0000

    Connect Kafka : JINO

--- src/main/java/miniprojectjo/infra/KafkaTestController.java
+++ src/main/java/miniprojectjo/infra/KafkaTestController.java
@@ -1,25 +1,26 @@
 package miniprojectjo.infra;
 
+import lombok.RequiredArgsConstructor;
+import miniprojectjo.domain.PublishingRequested;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import miniprojectjo.domain.PublishingRequested;
+import java.util.Date;
 
 @RestController
 @RequestMapping("/kafka-test")
+@RequiredArgsConstructor
 public class KafkaTestController {
-    
+
     @Autowired
     private KafkaTemplate<String, Object> kafkaTemplate;
-    
+
     @PostMapping("/publish")
-    public String publishTestEvent(@RequestBody PublishingRequested event) {
-        event.setEventType("PublishingRequested"); // 중요: 헤더 대신 객체 속성으로 처리
+    public String publish(@RequestBody PublishingRequested event) {
+        event.setEventType("PublishingRequested");  // 중요: 이벤트 타입
+        event.setCreatedAt(new Date());
         kafkaTemplate.send("miniprojectjo", event);
-        return "Sent PublishingRequested event: " + event.getId();
+        return "Event published: " + event.getTitle();
     }
 }
